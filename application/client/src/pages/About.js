18eda7bf9d13c42b171d412b6e317d46248ade6f
Yan-XYR <amandax2013.ax@gmail.com>
2020-02-17 18:45:34 -0800

    Yan's Milestone0

--- application/client/src/pages/About.js
+++ application/client/src/pages/About.js
@@ -7,72 +7,72 @@ import JunMinLi from './JunMinLi';
 import './css/About.css';
 
 const About = () => {
-  return (
-    <Container>
-      <Row>
-        <Col lg={{ span: 8, offset: 4 }}>
-          <Label><b>Software Engineering class SFSU<br/>
-             Spring 2020<br/>
-             Section 01<br/>
-             Team 02</b></Label>
-        </Col>
-      </Row><hr/>
-      <Row>
-          <b>Our team members</b>
-      </Row>
-      <br/>
-      <Row>
-          <Link to="/KevinLuong">
-            <Button variant = "secondary">
-                Kevin Luong
-            </Button>
-        </Link>
-     </Row>
-     <Row>
-          <Link to="/JunMinLi">
-            <Button variant = "secondary">
-                JunMinLi
-            </Button>
-        </Link>
-     </Row>
-     <Row>
-          <Link to="/JunMinLi">
-            <Button variant = "secondary">
-                member
-            </Button>
-        </Link>
-     </Row>
-     <Row>
-          <Link to="/JunMinLi">
-            <Button variant = "secondary">
-                member
-            </Button>
-        </Link>
-     </Row>
-     <Row>
-          <Link to="/JunMinLi">
-            <Button variant = "secondary">
-                member
-            </Button>
-        </Link>
-     </Row>
-     <Row>
-          <Link to="/JunMinLi">
-            <Button variant = "secondary">
-                member
-            </Button>
-        </Link>
-     </Row>
-     
-      <Switch>
-        <Route exact path="/JunMinLi" component={JunMinLi} />
-      </Switch>
-    </Container>
+    return (
+        <Container>
+            <Row>
+                <Col lg={{ span: 8, offset: 4 }}>
+                    <Label><b>Software Engineering class SFSU<br/>
+                        Spring 2020<br/>
+                        Section 01<br/>
+                        Team 02</b></Label>
+                </Col>
+            </Row><hr/>
+            <Row>
+                <b>Our team members</b>
+            </Row>
+            <br/>
+            <Row>
+                <Link to="/KevinLuong">
+                    <Button variant = "secondary">
+                        Kevin Luong
+                    </Button>
+                </Link>
+            </Row>
+            <Row>
+                <Link to="/JunMinLi">
+                    <Button variant = "secondary">
+                        JunMinLi
+                    </Button>
+                </Link>
+            </Row>
+            <Row>
+                <Link to="/JunMinLi">
+                    <Button variant = "secondary">
+                        member
+                    </Button>
+                </Link>
+            </Row>
+            <Row>
+                <Link to="/YanruiXu">
+                    <Button variant = "secondary">
+                        YanruiXu
+                    </Button>
+                </Link>
+            </Row>
+            <Row>
+                <Link to="/JunMinLi">
+                    <Button variant = "secondary">
+                        member
+                    </Button>
+                </Link>
+            </Row>
+            <Row>
+                <Link to="/JunMinLi">
+                    <Button variant = "secondary">
+                        member
+                    </Button>
+                </Link>
+            </Row>
+
+            <Switch>
+                <Route exact path="/JunMinLi" component={JunMinLi} />
+            </Switch>
+        </Container>
     );
 };
 const mapStateToProps = state => ({
 
-  isLoggedIn: state.userReducer.isLoggedIn,
+    isLoggedIn: state.userReducer.isLoggedIn,
 
 })
 export default connect(mapStateToProps)(About);
