239932802d3a196384e083a5f0360a0546ed1b4a
kev <frfkevin>
2020-03-11 22:36:33 -0700

    boilerplate sfsuaccess files

---
+++ application/M2/SFSUAccess/flask-backend/static/react/precache-manifest.cd3b634281a94aa3f23a77f9ea0331e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed76c587de69bba77b4f5ad5f30ad65",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "08c9b25c0cd7eb009d75",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1c3a0c7bb16963a4e203",
+    "url": "/static/react/js/2.58a5c625.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/react/js/2.58a5c625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c9b25c0cd7eb009d75",
+    "url": "/static/react/js/main.bba622ce.chunk.js"
+  },
+  {
+    "revision": "165304c935f15a4707af",
+    "url": "/static/react/js/runtime-main.7ca781aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
