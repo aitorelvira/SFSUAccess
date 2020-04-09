5b5e7acc6288b8a8c763b4c2b25869a3c8745e9e
kev <kluong4@mail.sfsu.edu>
2020-04-08 19:33:46 -0700

    Cleaned up back end code, added app routes for more functionality
    post,put,delete for Items and Users. Added status codes and

--- application/M3/SFSUAccess/flask-backend/main.py
+++ application/M3/SFSUAccess/flask-backend/main.py
@@ -1,160 +1,134 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file, Response
 import pymysql
 
+#TODO for back end team, check all to dos in file... add response status codes to any that are missing
+
+
+
+# Connect to the database
+connection = pymysql.connect(host='csc648.cxyapjc8a04v.us-west-1.rds.amazonaws.com',
+                             user='admin',
+                             password='rdsmysql',
+                             db='fixedAGAIN',
+                             cursorclass=pymysql.cursors.DictCursor)
+cur = connection.cursor()
 app = Flask(__name__)
 
+#products / categories / searches
+@app.route('/search')
+def get_categories():
+    cur.execute("SELECT product_category_name FROM product_categories LIMIT 50")
+    results = cur.fetchall()
+    return jsonify(results)
 
-class Database:
-    def __init__(self):
-        host = "csc648.cxyapjc8a04v.us-west-1.rds.amazonaws.com"
-        user = "admin"
-        password = "rdsmysql"
-        db = "proddb"
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
-                                   DictCursor, autocommit=True)
-        self.cur = self.con.cursor()
+#this function can be made shorter if I used the product category name as the foreign key of products
+#that way I can just select using the url category
+#TODO front end needs to adapt for GET and POST
+@app.route('/search/<category>',methods=['GET','POST'])
+def get_category_items(category):
+    if request.method=='GET':
+        sql = "SELECT * FROM products WHERE product_category = %s"
+        cur.execute(sql,(category))
+        results = cur.fetchall()
+        return jsonify(results)
+    if request.method=='POST':
+        search_query = request.get_json()
+        sql = "SELECT * FROM products WHERE product_category = %s AND product_name LIKE %s"
+        cur.execute(sql,(category,str(search_query['search_query'])))
+        results = cur.fetchall()
+        return jsonify(results)
 
-    def list_categories(self):
-        self.cur.execute("SELECT product_category_name FROM product_categories LIMIT 50")
-        result = self.cur.fetchall()
-        return result
-
-    def list_members(self):
-        self.cur.execute("SELECT first_name FROM Team_Members LIMIT 50")
-        result = self.cur.fetchall()
-        return result
-
-    def list_category_entries(self, categoryname):
-        query = "SELECT * FROM " + str(categoryname) + "_products LIMIT 50"
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        return result
-
-    def list_all_category_entries(self, paramobjects):
-        product_categories = ["class_notes", "video", "music"]
-        product_fields = ["product_name", "product_file_size", "product_description", "product_author"]
-        result = []
-        for category in product_categories:
-            if "search_query" in paramobjects and len(paramobjects) == 1:
-                for field in product_fields:
-                    query = "SELECT * FROM " + category + "_products WHERE " + field + " LIKE '%" + \
-                            paramobjects[
-                                "search_query"] + "%' LIMIT 50"
-                    self.cur.execute(query)
-                    result += self.cur.fetchall()
-                return result
-            elif len(paramobjects) == 0:
-                query = "SELECT * FROM " + category + "_products LIMIT 50"
-                self.cur.execute(query)
-                result += self.cur.fetchall()
-            for field in product_fields:
-                if field in paramobjects:
-                    query = "SELECT * FROM " + category + "_products WHERE " + field + " LIKE '%" + \
-                            paramobjects[
-                                field] + "%' LIMIT 50"
-                    self.cur.execute(query)
-                    result += self.cur.fetchall()
-        return result
-
-    # this function will return any entry where any field matches the search_query url parameter or if a field is specified. FROM SPECIFIED CATEGORY
-    def list_search_query_entries(self, categoryname, paramsobject):
-        product_fields = ["product_name", "product_file_size", "product_description", "product_author"]
-
-        if "search_query" in paramsobject and len(paramsobject) == 1:
-            result = []
-            for field in product_fields:
-                query = "SELECT * FROM " + str(categoryname) + "_products WHERE " + field + " LIKE '%" + paramsobject[
-                    "search_query"] + "%' LIMIT 50"
-                self.cur.execute(query)
-                result += self.cur.fetchall()
-            return result
-        for field in product_fields:
-            if field in paramsobject:
-                query = "SELECT * FROM " + str(categoryname) + "_products WHERE " + field + " LIKE '%" + paramsobject[
-                    field] + "%' LIMIT 50"
-            self.cur.execute(query)
-        return self.cur.fetchall()
-
-    def check_is_unregistered(self, email):
-        query = "SELECT email FROM registered_users WHERE email ='" + email + "'"
-        return not bool(self.cur.execute(query))
-
-    def check_is_registered(self, email):
-        query = "SELECT email FROM registered_users WHERE email ='" + email + "'"
-        return bool(self.cur.execute(query))
-
-    def authenticate_login(self, content):
-        query = "SELECT email,first_name,last_name,id FROM registered_users WHERE email ='" + content[
-            'email'] + "' AND password ='" + content['password'] + "'"
-        if bool(self.cur.execute(query)):
-            return self.cur.fetchall()
-
-    def register_user(self, registration_info):
-        query = "INSERT INTO registered_users(id,email,password,first_name,last_name) VALUES (0,'" + registration_info[
-            'email'] + "','" + registration_info['password'] + "','" + registration_info['first_name'] + "','" + \
-                registration_info['last_name'] + "')"
-        print(query)
-        print(self.cur.execute(query))
-
-
-# this function returns all requested data searched from given category
-@app.route('/api/search/<category>')
-def get_search(category):
-    db = Database()
-    paramsobject = request.args
-    fixed_case = str(category.lower())
-    if category == "all" or category == "All":
-        emps = db.list_all_category_entries(paramsobject)
-        return jsonify(emps)
-    # capitalize all first letters of a category
-    # fixed_case = fixed_case.capitalize()
-    print(fixed_case)
-    if len(paramsobject) == 0:
-        emps = db.list_category_entries(fixed_case)
+@app.route('/register', methods=['POST'])
+def register_user():
+#     this block of comment is if we get around to fixing React to post a FORM instead of JSON
+#     email = request.form['email']
+#     password = request.form['password']
+#     first_name = request.form['first_name']
+#     last_name = request.form['last_name']
+    form = request.get_json()
+    email = form['email']
+    password = form['password']
+    first_name = form['first_name']
+    last_name = form['last_name']
+    # TODO Add functionality to discern from Student to Faculty, need React team to update form to reflect that
+    #check if unregistered first
+    sql = "SELECT email FROM registered_users WHERE email = %s"
+    if not cur.execute(sql,email):
+        #user is not registered yet, may proceed with registration, TODO 2 as the final value sets privelege_type to Student by default
+        sql = "INSERT INTO registered_users(email,password,first_name,last_name,privelege_type) VALUES (%s,%s,%s,%s,2)"
+        cur.execute(sql,(email,password,first_name,last_name))
+        connection.commit()
+        status_code = Response(status=201)
+        return status_code
     else:
-        emps = db.list_search_query_entries(fixed_case, paramsobject)
-    return jsonify(emps)
+        #user is registered already
+        status_code = Response(status=409)
+        return status_code
 
-
-# this function returns the list of categories
-@app.route('/api/search')
-def list_categories():
-    db = Database()
-    emps = db.list_categories()
-    return jsonify(emps)
-
-
-@app.route('/api/register', methods=['POST'])
-def registerNewUser():
-    db = Database()
-    content = request.get_json()
-    if db.check_is_unregistered(content['email']):
-        db.register_user(content)
-        #return content['email'] + " has been registered"
-        return content['email']
+@app.route('/login', methods=['POST'])
+def login():
+    form = request.get_json()
+    email = form['email']
+    password = form['password']
+    sql = "SELECT email,first_name,last_name,id,privelege_type FROM registered_users WHERE email =%s AND password =%s"
+    if bool(cur.execute(sql,(email,password))):
+        #log in successful
+        return jsonify(cur.fetchall()),202
     else:
-        return ""
-        #return content['email'] + " is already registered,check email and try again"
+        #information incorrect
+        status_code = Response(status=401)
+        return status_code
 
+#@app.route('/account')
 
-@app.route('/api/login', methods=['POST'])
-def loginUser():
-    db = Database()
-    content = request.get_json()
-    if db.check_is_registered(content['email']):
-        return jsonify(db.authenticate_login(content))
+#all user listings, active or pending
+@app.route('/user_listings', methods=['POST'])
+def user_listings():
+    form = request.get_json()
+    email = form['email']
+    sql = "SELECT id FROM registered_users WHERE email = %s"
+    #checking if this email exists
+    if not bool(cur.execute(sql,(email))):
+        #user does not exist
+        status_code = Response(status=204)
+        return status_code
+    #else moving along
+    user_row = cur.fetchall()
+    user_id = user_row[0]['id']
+    sql = "SELECT * FROM products WHERE registered_user_id = %s"
+    if bool(cur.execute(sql,(user_id))):
+        #user has items
+        return jsonify(cur.fetchall()),302
+    else:
+        #users does not have items
+        status_code = Response(status=204)
+        return status_code
 
+@app.route('/product',methods=['POST'])
+def post_product():
+    # create product post TODO FOR KEVIN still need to figure out file system
+    product_name = request.form['product_name']
+    product_category = request.form['product_category']
+    product_author = request.form['product_author']
+    product_description = request.form['product_description']
+    product_user_id = request.form['user_id']
+    product_license = request.form['product_license']
+    sql = "INSERT INTO products(product_name,product_category,product_author,product_description,registered_user_id,product_license) VALUES (%s,%s,%s,%s,%s,%s)"
+    cur.execute(sql,(product_name,product_category,product_author,product_description,product_user_id,product_license))
+    connection.commit()
+    status_code = Response(status=201)
+    return status_code
 
-# this is for the flask team to test new functionality easily by calling the /api/test endpoint
-# this function will only test whatever code is inside test(). you are welcome to erase the definition to test your own
-@app.route('/api/test')
-def test():
-    db = Database()
-    paramsobject = request.args['search_query']
-    emps = db.list_all_category_entries(paramsobject)
-    return jsonify(emps)
+@app.route('/product/<id>', methods=['PUT','DELETE'])
+def manage_product(id):
+    if request.method=='PUT':
+        #check if product id still exists, if so move along
+        # update product post TODO
+        return "put test"
+    if request.method=='DELETE':
+        #check if product id still exists, if so move along TODO
+        # delete product post
+        return "delete test"
 
-
-
-# app run
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
