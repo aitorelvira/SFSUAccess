9e98825343b184257d1e858ac58b0f1987970ace
kev <kluong4@mail.sfsu.edu>
2020-05-13 09:59:12 -0700

    included read status boolean, latest message column, date time sent

--- application/M3/SFSUAccess/flask-backend/flask-blueprints/messages/messages_bp.py
+++ application/M3/SFSUAccess/flask-backend/flask-blueprints/messages/messages_bp.py
@@ -13,15 +13,18 @@ messages_bp = Blueprint('messages_bp', import_name=__name__)
 @messages_bp.route("/messages", methods=['POST'], defaults={'message_thread_id': None})
 @messages_bp.route("/messages/<int:message_thread_id>/", methods=['GET'])
 def view_inbox_conversations(message_thread_id):
-    request_content = request.get_json()
-    inbox_user_id = request_content["user_id"]
-    print(inbox_user_id)
     if message_thread_id != None:
+        sql = "update messages set read_status=1 where message_thread_id =%s"
+        cur.execute(sql,message_thread_id)
+        connection.commit()
         sql = "select * from messages WHERE message_thread_id=%s"
         cur.execute(sql, (message_thread_id))
         data = cur.fetchall()
         return jsonify(data), 200
     else:
+        request_content = request.get_json()
+        inbox_user_id = request_content["user_id"]
+        print(inbox_user_id)
         sql = "select * from message_threads WHERE buyer_id=%s OR seller_id=%s"
         cur.execute(sql, (inbox_user_id, inbox_user_id))
         data = cur.fetchall()
@@ -32,14 +35,14 @@ def view_inbox_conversations(message_thread_id):
 def reply_to_post():
     form = request.get_json()
     sender_user_id = form["sender_user_id"]
-    recipient_user_id = rform["recipient_user_id"]
+    recipient_user_id = form["recipient_user_id"]
     message_contents = form["message_contents"]
     product_id = form["product_id"]
-    sql = "insert into message_threads(buyer_id,seller_id,product_id) values (%s,%s,%s)"
-    cur.execute(sql, (sender_user_id, recipient_user_id, product_id))
+    sql = "insert into message_threads(buyer_id,seller_id,product_id,last_message) values (%s,%s,%s,%s)"
+    cur.execute(sql, (sender_user_id, recipient_user_id, product_id,message_contents))
     connection.commit()
     message_thread_id = cur.lastrowid
-    sql = "insert into messages(sender_user_id,recipient_user_id,message_thread_id,message_contents) values (%s,%s,%s,%s)"
+    sql = "insert into messages(sender_user_id,recipient_user_id,message_thread_id,date_time_sent,message_contents) values (%s,%s,%s,NOW(),%s)"
     cur.execute(sql, (sender_user_id, recipient_user_id, message_thread_id, message_contents))
     connection.commit()
     status_code = Response(status=200)
@@ -48,15 +51,18 @@ def reply_to_post():
 #replying to an existing conversation
 @messages_bp.route('/messages/<int:message_thread_id>/reply/', methods=['POST'])
 def reply_to_thread(message_thread_id):
-    sender_user_id = request.form["sender_user_id"]
-    recipient_user_id = request.form["recipient_user_id"]
-    message_contents = request.form["message_contents"]
-    sql = "insert into messages(sender_user_id,recipient_user_id,message_thread_id,message_contents) values (%s,%s,%s,%s)"
+    form = request.get_json()
+    sender_user_id = form["sender_user_id"]
+    recipient_user_id = form["recipient_user_id"]
+    message_contents = form["message_contents"]
+    sql = "insert into messages(sender_user_id,recipient_user_id,message_thread_id,date_time_sent,message_contents) values (%s,%s,%s,NOW(),%s)"
     cur.execute(sql, (sender_user_id, recipient_user_id, message_thread_id, message_contents))
+    connection.commit()
+    sql = "update message_threads set last_message=%s where id = %s"
+    cur.execute(sql,(message_contents,message_thread_id))
     connection.commit()
     status_code = Response(status=200)
     return status_code
-
 
 # DELETE THE WHOLE THREAD
 @messages_bp.route('/messages/<int:message_thread_id>', methods=['DELETE'])
@@ -65,7 +71,6 @@ def delete_message_thread(message_thread_id):
     sql = "delete from messages where message_thread_id=%s"
     cur.execute(sql, message_thread_id)
     connection.commit()
-
     sql = "delete from message_threads where id=%s"
     cur.execute(sql, message_thread_id)
     connection.commit()
